b90de950c74d205694c525f917609893d2e0a839
Mahmoud Zeidan <mahmoudzeidan10@gmail.com>
2020-05-27 16:29:53 +0300

    adding total vert

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var parentElement = document.getElementById('container');
 var tableEl = document.createElement('table');
 parentElement.appendChild(tableEl);
 
-var hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'] ;
+var hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm' , 'Total'] ;
 var shops =[ ];
 
 function Store(name, minCustomerPerHour, maxCustomerPerHour, averageCookierPerPerson){
